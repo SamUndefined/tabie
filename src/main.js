41a8b3eb1e079d584077a664c250c8cc8e496452
SamUndefined <sam.undefined.dev@gmail.com>
2019-05-26 17:35:37 -0500

    Add page tracking, maybe

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import '~/assets/style/index.scss'
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
+import VueAnalytics from 'vue-analytics'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
   
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(VueAnalytics, {
+    id: 'UA-140891510-1',
+    router
+  })
 }
